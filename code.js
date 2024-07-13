142fb822e0d74d6a35c05e5ff42d59060a1a3c7d
Muhammad Saad <sadsaadmuhammad@gmail.com>
2024-07-13 02:27:56 -0600

    OK ok

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 let humanScore = 0
 let compScore = 0
+const results = document.getElementById("textForResults")
 
 function getCompChoice(){
     let compChoice = Math.floor(Math.random()*3)
@@ -10,22 +11,7 @@ function getCompChoice(){
     } else {
     return "Scissor" }
   }
-//what if i do this then commit?
-function getHumanChoice (x){
-    let choose = ("What are you choosing!")
-    choose = choose.toLowerCase()
 
-
-    if (choose === "rock"){
-        return "Rock"
-    } else if (choose === "paper"){
-        return "Paper"
-    } else if (choose === "scissor" || choose === "scissors"){
-        return "Scissor"
-    } else {
-        return "wrong"}
-
-}
 
 function playRound (humanChoice, computerChoice) {
     if( humanChoice === computerChoice){
@@ -33,56 +19,57 @@ function playRound (humanChoice, computerChoice) {
     } else if (humanChoice === "Rock" && computerChoice === "Scissor" ||
                 humanChoice === "Scissor" && computerChoice === "Paper" ||
                 humanChoice === 'Paper' && computerChoice === "Rock"
-    ){
-        humanScore += 1  
+    ){ 
         return "You won";
     } else {
-        compScore += 1;
         return "You lose"
 } 
 }
 
 
+function playGames (humanChoice){
+    const compChoice = getCompChoice();
+    const roundResult = playRound(humanChoice, compChoice);
+    updateScores(roundResult);
+
+    displayResults(humanChoice, compChoice, roundResult);
+}
 
 
-function playGames (x){
-
-    for (i = 0 ;i < x ;i++){
-        let humanChoice = getHumanChoice();
-        let compChoice = getCompChoice();
-        const roundResult = playRound(humanChoice, compChoice)
-        console.log(`${i + 1} Round`);
-        console.log(`you chose: ${humanChoice}`)
-        console.log(`the computer chose: ${compChoice} : ${roundResult}`)
-        console.log(`the current socre for you is: ${humanScore}`)
-        console.log(`the current socre for the Computer is: ${compScore}`)
-        console.log("~~~~~~~~~~~~~")
-        
-    } if (humanScore > compScore){
-        console.log("You Won!!!")
-    } if (compScore > humanScore){
-        console.log("You Lose :(")
-    } if (humanScore == compScore){
-        console.log("Whew! close game")
-    } const playAgain = confirm("Would you like to go again?")
-    if (playAgain){
-        humanScore = 0, 
-        compScore = 0,
-        playGames(5)
-    } else (
-        console.log('------------'),
-        console.log("Come Again!")
-    )
-}  
-
-playGames(1);
 
 
-const rockButton = document.getElementById("chooseRock")
-const paperButton = document.getElementById("choosePaper")
-const scissorButton = document.getElementById("chooseScissor")
+// -----------
 
-    function chooseRock(){
-        getHumanChoice() === "Rock"
+function buttons(){
+    const rockButton = document.getElementById("chooseRock")
+    const paperButton = document.getElementById("choosePaper")
+    const scissorButton = document.getElementById("chooseScissor")
+
+        rockButton.addEventListener('click',e =>{
+            playGames('Rock')
+
+    });
+        paperButton.addEventListener('click',e =>{
+            playGames('paper')
+    });
+        scissorButton.addEventListener('click',e =>{
+            playGames('Scissor')
+    });
+    
+}
+
+
+function updateScores(result) {
+    if (result === "You won") {
+        humanScore++;
+    } else if (result === "You lose") {
+        compScore++;
     }
-// 
+}
+
+function displayResults(humanChoice, compChoice, roundResult) {
+    const roundNumber = humanScore + compScore;
+    results.innerHTML += `<p>Round ${roundNumber}: You chose ${humanChoice}, Computer chose ${compChoice}. Result: ${roundResult}. Your score: ${humanScore}, Computer's score: ${compScore}</p>`;
+}
+
+buttons()
